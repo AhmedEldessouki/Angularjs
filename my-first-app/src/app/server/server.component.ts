023ff071fcde1fbbaa6deac02662d5eb915f02bb
Nemo <Nemo@NEMO>
2020-07-06 10:29:12 +0300

    learing about components in Angularjs
    1) the app folder holds the project files
    2)componen name always sart with CAP letter.
    3)after creating a new component. you will need o add it to app.modules so the app knows that it exists
    4) you can create a new component using the terminal "ng g c + 'the component name'" (it also declares the new component in the app.ts file)
    
    note: the .spec file is for code testing.
    
    5) the template or templateUrl must be in the component.ts

---
+++ my-first-app/src/app/server/server.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  // selector's name sould always be unique
+  selector: 'app-server',
+  templateUrl: './server.component.html',
+})
+export class ServerComponent {}
