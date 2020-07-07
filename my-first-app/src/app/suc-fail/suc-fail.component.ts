72f05e1bd41d1670ef5dc46015bedffa31f296e5
Nemo <Nemo@NEMO>
2020-07-07 10:06:59 +0300

    practice

---
+++ my-first-app/src/app/suc-fail/suc-fail.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-suc-fail',
+  templateUrl: './suc-fail.component.html',
+  styleUrls: ['./suc-fail.component.scss'],
+})
+export class SucFailComponent {}
