6cf70a13987bf3e36c651d16a78d40eb56d83388
Nemo <Nemo@NEMO>
2020-07-11 12:44:52 +0300

    app created

--- databinding-and-components/src/app/app.component.ts
+++ databinding-and-components/src/app/app.component.ts
@@ -3,8 +3,29 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'databinding-and-components';
+  serverElements = [];
+  newServerName = '';
+  newServerContent = '';
+
+  // tslint:disable-next-line: typedef
+  onAddServer() {
+    this.serverElements.push({
+      type: 'server',
+      name: this.newServerName,
+      content: this.newServerContent,
+    });
+  }
+
+  // tslint:disable-next-line: typedef
+  onAddBlueprint() {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: this.newServerName,
+      content: this.newServerContent,
+    });
+  }
 }
